12aef5c8ecbd8ce656a9a8c15cbb1cd73a72aa13
Tomer Simis <tdl.simis@gmail.com>
2015-03-23 15:54:32 -0300

    Update 20150324.hs

--- 20150324.hs
+++ 20150324.hs
@@ -9,7 +9,7 @@ merge left right
 mergesort :: [Int] -> [Int]
 mergesort [] = []
 mergesort [x] = [x]
-mergesort ls = merge (mergesort(fst (splitAt ((length ls) `div` 2) ls))) (mergesort(snd (splitAt ((length ls) `div` 2) ls)))
+mergesort ls = merge (mergesort(fst (mySplitAt ((length ls) `div` 2) ls))) (mergesort(snd (mySplitAt ((length ls) `div` 2) ls)))
 
 
 -- QUICKSORT
@@ -20,7 +20,7 @@ right :: Int -> Int
 right i = (2*i + 1) 
 
 swap :: Int -> Int -> [Int] -> [Int] -- 2 4 [1,5,7,9,3,2]
-swap i j ls = (take (i-1) ls) ++ (ls!!(j-1):[]) ++ (drop (i) (take (j-1) ls) ) ++ (ls!!(i-1):[]) ++ (drop j (take (length ls) ls) )
+swap i j ls = (myTake (i-1) ls) ++ (ls!!(j-1):[]) ++ (myDrop (i) (myTake (j-1) ls) ) ++ (ls!!(i-1):[]) ++ (myDrop j (myTake (length ls) ls) )
 
 heapify :: Int -> [Int] -> [Int]
 heapify i ls
@@ -49,3 +49,23 @@ heapsort ls
 	| ls == [] = []
 	| otherwise = head (buildHeap ((length ls) `div` 2) ls):heapsort (goDown 1 (tail (buildHeap ((length ls) `div` 2) ls)))
 
+
+mySplitAt :: Int -> [Int] -> ([Int],[Int])
+mySplitAt n ls = (take n ls, drop n ls)
+
+myTake :: Int -> [Int] -> [Int]
+myTake n ls = (myTake' 0 n ls)
+
+myTake' :: Int -> Int -> [Int] -> [Int]
+myTake' i n ls
+	| i >= n = []
+	| i < n = (head ls):(myTake' (i+1) n (tail ls))
+
+myDrop :: Int -> [Int] -> [Int]
+myDrop n ls = (myDrop' 0 n (length ls) ls)
+
+myDrop' :: Int -> Int -> Int -> [Int] -> [Int]
+myDrop' i n len ls
+	| i >= n && i < len = (head ls):(myDrop' (i+1) n len (tail ls))
+	| i < n = myDrop' (i+1) n len (tail ls)
+	| otherwise = []
