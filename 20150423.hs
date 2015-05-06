73075b07ebeeafdf86a5f75273ccc5fea9db86d1
Tomer Simis <tdl.simis@gmail.com>
2015-05-06 01:09:02 -0300

    Update 20150423.hs

--- 20150423.hs
+++ 20150423.hs
@@ -8,3 +8,24 @@ splitWords' (a:as) acc
 	| a == ' ' && (length acc) == 0 = (splitWords' as "")
 	| otherwise = splitWords' as (acc++[a])
 	
+dropSpace :: String -> String
+dropSpace (a:as)
+	| a == ' ' = dropSpace as
+	| otherwise = a:as
+
+getWord :: String -> String
+getWord str = getWord' str ""
+
+getWord' :: String -> String -> String
+getWord' (a:as) acc
+	| a == ' ' = acc
+	| otherwise = getWord' as (acc++[a])
+
+dropWord :: String -> String
+dropWord (a:as)
+	| a == ' ' = as
+	| otherwise = dropWord as
+
+foldr' :: (a->b->b) -> b -> [a] -> b
+foldr' _ b [] = b
+foldr' f b (a:as) = f a (foldr' f b as)
