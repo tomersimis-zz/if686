78009ad3c2e353c7d67c3d624544178970b20f59
Tomer Simis <tls@cin.ufpe.br>
2015-07-11 13:01:29 -0300

    Update 20150602.java

--- 20150602.java
+++ 20150602.java
@@ -44,3 +44,65 @@ public class Exercicio4{
 		
 	
 }
+
+
+
+class Node{
+	
+	int val;
+	Node next;
+	
+	public Node(int n){
+		val = n;
+	}
+	
+}
+
+class SafeQueue{
+
+	public static boolean isWorking = false;
+	
+	private Node head, tail;
+	
+	public void add(int n){
+	
+		Node newNode = new Node(n);
+		
+		while(isWorking){}
+		isWorking = true;
+		
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+		
+		isWorking = false;
+	}
+	
+	public int pop() throws Exception{
+	
+		while(isWorking){}
+		
+		isWorking = true;
+		
+		if(head == null) throw new Exception("List empty");
+		
+		int t = head.val;
+		head = head.next;
+		
+		isWorking = false;
+		return t;
+		
+	}
+	
+	
+}
+
+public class Exercicio5{
+	
+		
+	
+}
