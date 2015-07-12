fe0dacf0b5fb0e70ddc0096d480a0377da0d0954
Tomer Simis <tls@cin.ufpe.br>
2015-07-12 13:50:34 -0300

    Update 20150317.hs

--- 20150317.hs
+++ 20150317.hs
@@ -7,3 +7,32 @@ qtd s n
 	
 vendas :: Int -> Int
 vendas n = n
+
+
+quicksort :: [Int] -> [Int]
+quicksort [] = []
+quicksort (a:as) = quicksort ([x | x <- as, x < a]) ++ (a:[x | x <- as, x == a]) ++ quicksort([x | x <- as, x > a])
+
+fibNeven :: Int -> [Int]
+fibNeven 0 = []
+fibNeven x = fibNeven' x 0
+
+fibNeven' :: Int -> Int -> [Int]
+fibNeven' 0 _ = []
+fibNeven' n a
+	| (f `mod` 2) == 0 = f:(fibNeven' (n-1) (a+1))
+	| otherwise = fibNeven' n (a+1)
+	where f = fib a
+
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1
+fib x = (fib $ x-1) + (fib $ x-2)
+
+orden :: [Int] -> [Int]
+orden [] = []
+orden (a:as) = (orden [x | x <- as, (sumd x) < (sumd a)]) ++ (a:[x | x <- as, (sumd x) == (sumd a)]) ++ (orden [x | x <- as, (sumd x) > (sumd a)])
+
+sumd :: Int -> Int
+sumd 0 = 0
+sumd x = (x `mod` 10) + (sumd (x `div` 10))
